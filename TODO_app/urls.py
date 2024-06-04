6c591facb922d26f575828098a3bce8000256c52
mtvzg <ziganshin.matvei@yandex.ru>
2024-06-04 19:56:25 +0500

    added update_task def to the views.py

--- TODO_app/urls.py
+++ TODO_app/urls.py
@@ -5,5 +5,6 @@ from TODO_app import views
 urlpatterns = [
     path('', views.todo_home_page, name='todo_home'),
     path('task/<int:task_id>/', views.task_status, name='task_status'),
-    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task')
+    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('update_task/<int:task_id>/', views.update_task, name='update_task')
 ]
--- TODO_app/views.py
+++ TODO_app/views.py
@@ -41,3 +41,19 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('todo_home')
+
+
+def update_task(request, task_id):
+    """Функция для редактирования задачи в модели"""
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_home')
+    else:
+
+        form = TaskForm(instance=task)
+        return render(request, 'todo_update_task.html', {
+            'form': form
+        })
